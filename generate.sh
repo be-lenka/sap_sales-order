bb6c652191ab10cac84f98f6fc2dc99a8a164557
Peter Veres <peter.veres@gmail.com>
2024-07-09 09:47:14 +0200

    doc

--- generate.sh
+++ generate.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-#usage:  ./generate.sh filename.json package_name NamespaceName
+usage(){
+ echo "
+🧰 Generate composer package from provided schema.json file
+
+__________________________________________________________________
+
+💡 usage:   ./generate.sh filename.json package_name NamespaceName
+▶️  example: ./generate.sh sales_order.json sales_order SalesOrder
+
+"
+}
 #example: ./generate.sh sales_order.json sales_order SalesOrder
+
+if [ "$#" -ne 3 ]; then
+	usage;
+	exit;
+fi
+if [ "$0" -eq "--help" ]; then
+	usage;
+	exit;
+fi
 
 mkdir -p resources
 docker run --user $(id -u):$(id -g) --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v7.0.0 generate -t /local/resources -i /local/$1 -g php -o /local/ -p licenseName="MIT" -p developerOrganization="BeLenka Dev" -p developerOrganizationUrl="https://www.belenka.com" -p composerPackageName=be-lenka/sap/$2 -p invokerPackage=BeLenka\\SAP\\$3
